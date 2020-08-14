3a11c0a0e4ca8e5714e594fd8dc751e9874bff3c
app fogtest <appfogtest8@mm.st>
2020-08-13 21:57:16 -0700

    Ch 20: Project set up

---
+++ app/src/main/java/com/exampler/podplay/ui/PodcastActivity.kt
@@ -0,0 +1,12 @@
+package com.exampler.podplay.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.exampler.podplay.R
+
+class PodcastActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_podcast)
+    }
+}

2cf6ddf968ff33d916f622283b38a1ed84a001b9
app fogtest <appfogtest8@mm.st>
2020-08-19 21:57:56 -0700

    Ch 20: Creating the podcast response model. End Ch20

--- app/src/main/java/com/exampler/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/exampler/podplay/ui/PodcastActivity.kt
@@ -2,11 +2,24 @@ package com.exampler.podplay.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.exampler.podplay.R
+import com.exampler.podplay.repository.ItunesRepo
+import com.exampler.podplay.service.ItunesService
 
 class PodcastActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_podcast)
+
+        val TAG = javaClass.simpleName
+
+        val itunesService = ItunesService.instance
+
+        val itunesRepo = ItunesRepo(itunesService)
+
+        itunesRepo.searchByTerm("Android Developer") {
+            Log.i(TAG, "Results = $it")
+        }
     }
 }

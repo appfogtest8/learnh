865a7b1b637a29b5cec5113d05de5e7a312af39b
app fogtest <appfogtest8@mm.st>
2020-08-25 13:45:15 -0700

    Ch 21: Loading the options menu

--- app/src/main/java/com/exampler/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/exampler/podplay/ui/PodcastActivity.kt
@@ -1,8 +1,12 @@
 package com.exampler.podplay.ui
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import com.exampler.podplay.R
 import com.exampler.podplay.repository.ItunesRepo
 import com.exampler.podplay.service.ItunesService
@@ -21,5 +25,20 @@ class PodcastActivity : AppCompatActivity() {
         itunesRepo.searchByTerm("Android Developer") {
             Log.i(TAG, "Results = $it")
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+
+        val searchMenuItem = menu?.findItem(R.id.search_item)
+        val searchView = searchMenuItem?.actionView as SearchView
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        return true
     }
 }

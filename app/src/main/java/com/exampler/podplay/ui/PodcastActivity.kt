f33ac80e8453183291dc4a40edb59c45b7e05f80
app fogtest <appfogtest8@mm.st>
2020-08-28 13:32:09 -0700

    Ch 21: Implementing the search

--- app/src/main/java/com/exampler/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/exampler/podplay/ui/PodcastActivity.kt
@@ -2,6 +2,7 @@ package com.exampler.podplay.ui
 
 import android.app.SearchManager
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,19 +13,13 @@ import com.exampler.podplay.repository.ItunesRepo
 import com.exampler.podplay.service.ItunesService
 
 class PodcastActivity : AppCompatActivity() {
+
+    private val TAG = javaClass.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_podcast)
 
-        val TAG = javaClass.simpleName
-
-        val itunesService = ItunesService.instance
-
-        val itunesRepo = ItunesRepo(itunesService)
-
-        itunesRepo.searchByTerm("Android Developer") {
-            Log.i(TAG, "Results = $it")
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -40,5 +35,28 @@ class PodcastActivity : AppCompatActivity() {
         searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
 
         return true
+    }
+
+    private fun performSearch(term: String) {
+        val itunesService = ItunesService.instance
+        val itunesRepo = ItunesRepo(itunesService)
+
+        itunesRepo.searchByTerm(term) {
+            Log.i(TAG, "Results = $it")
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY) ?:
+                    return
+            performSearch(query)
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
     }
 }

1b03489e3ef07dbcb5db79d4a70538edad38ef40
app fogtest <appfogtest8@mm.st>
2020-09-10 16:44:22 -0700

    Ch 22: Podcast models

---
+++ app/src/main/java/com/exampler/podplay/model/Podcast.kt
@@ -0,0 +1,13 @@
+package com.exampler.podplay.model
+
+import java.util.*
+
+data class Podcast(
+
+    var feedUrl: String = "",
+    var feedTitle: String = "",
+    var feedDesc: String = "",
+    var imageUrl: String = "",
+    var lastUpdated: Date = Date(),
+    var episodes: List<Episode> = listOf()
+)
